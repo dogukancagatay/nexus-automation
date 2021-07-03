c39685a5972aec03f0ffcce8bf1277cb42f8a789
Doğukan Çağatay <dcagatay@gmail.com>
2021-07-03 16:48:10 +0300

    Add new raw repos

--- scripts/addRawRepos.groovy
+++ scripts/addRawRepos.groovy
@@ -1,15 +1,35 @@
 import org.sonatype.nexus.blobstore.api.BlobStoreManager
+import org.sonatype.nexus.repository.Repository
 
 // create a new blob store dedicated to usage with raw repositories
 // def blobstore = blobStore.createFileBlobStore('raw', 'raw').name
 def blobstore = BlobStoreManager.DEFAULT_BLOBSTORE_NAME
+def contentMaxAge = -1
+def negativeCacheTTL = 1
+def metadataMaxAge = 1440
 def strictContentTypeValidation = false
 
 repository.createRawHosted('file-private', blobstore)
 repository.createRawHosted('file-public', blobstore)
 
-repository.createRawProxy('github.com', 'https://github.com', blobstore, strictContentTypeValidation)
-repository.createRawProxy('raw.githubusercontent.com', 'https://raw.githubusercontent.com', blobstore, strictContentTypeValidation)
-repository.createRawProxy('downloads.apache.org', 'https://downloads.apache.org', blobstore, strictContentTypeValidation)
+def proxyMap = [
+  'github.com': 'https://github.com',
+  'raw.githubusercontent.com': 'https://raw.githubusercontent.com',
+  'downloads.apache.org': 'https://downloads.apache.org',
+  'update.k3s.io': 'https://update.k3s.io',
+  'storage.googleapis.com': 'https://storage.googleapis.com',
+]
 
-log.info('Script rawRepositories completed successfully')
+proxyMap.each{entry ->
+  log.info("Adding raw proxy $entry.key: $entry.value")
+  Repository repo = repository.createRawProxy(entry.key, entry.value, blobstore, strictContentTypeValidation)
+
+  if (repo != null) {
+    log.info("Settings attributes for $repo.name")
+    repo.getConfiguration().getAttributes().'proxy'.'contentMaxAge' = contentMaxAge
+    repo.getConfiguration().getAttributes().'proxy'.'metadataMaxAge' = metadataMaxAge
+    repo.getConfiguration().getAttributes().'negativeCache'.'timeToLive' = negativeCacheTTL
+  }
+}
+
+log.info('Script completed successfully')
